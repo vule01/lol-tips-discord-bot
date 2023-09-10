958797e5dc436d1e698e3e88aaf2ae041b250334
Vivianne Le <viviannele2001@gmail.com>
2023-09-09 23:01:32 -0700

    registered slash command for champion lore

---
+++ src/register-commands.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
+
+const commands = [
+    {
+        name: 'lore',
+        description: 'Displays the champion\'s lore.',
+        options: [
+            {
+                name: 'champion-name',
+                description: 'The name of the champion.',
+                type: ApplicationCommandOptionType.String,
+                required: true,
+            }
+        ]
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+
+const registerCommands = async () => {
+    try {
+        console.log('Registering slash commands...');
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        )
+
+        console.log(`Slash commands were registered successfully`);
+    } catch (err) {
+        console.log(`There was an error: ${err}`);
+    }
+};
+
+module.exports = { registerCommands };
